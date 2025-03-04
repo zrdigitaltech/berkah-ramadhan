ce0c00f8a3fe1dc11fb5bd7264b061237eb3594e
zikriramdani <zikriramdani@gmail.com>
2025-03-04 21:08:51 +0700

    Prod

--- src/app/components/SearchBar/index.js
+++ src/app/components/SearchBar/index.js
@@ -1,16 +1,29 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 const SearchBar = (props) => {
   const { value, onChange, onSubmit } = props;
 
+  const texts = ["K", "Ke", "Ket", "Keti", "Ketik", "Ketik d", "Ketik di", "Ketik dis", "Ketik disi", "Ketik disin", "Ketik disini ..."];
+  const [placeholder, setPlaceholder] = useState("");
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setPlaceholder(texts[index]);
+      index = (index + 1) % texts.length;
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <form className="forms" onSubmit={onSubmit}>
       <div className="input-group">
         <BiSearch className="search-icon" />
-        <input type="search" placeholder="Ketik disini ..." value={value} onChange={onChange} />
+        <input type="search" placeholder={placeholder} value={value} onChange={onChange} />
         <button type="submit" className="btn btn-search">
           <BiSearch color="#fff" size="1.7rem" />
         </button>
