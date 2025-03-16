cf4f18a5173ec273a7e16d3b08ee3949a8eac31d
zikriramdani <zikriramdani@gmail.com>
2025-03-16 11:43:43 +0700

    Fixing Order

--- src/app/[whatsApp]/page.js
+++ src/app/[whatsApp]/page.js
@@ -3,7 +3,7 @@ import Page from "./index";
 
 export async function generateStaticParams() {
   // Contoh: ambil daftar WhatsApp ID dari API atau database
-  const waIds = ['6281228883616']; // Ganti dengan ID yang valid
+  const waIds = ['6281228883616', '628977663923']; // Ganti dengan ID yang valid
   
   return waIds.map((id) => ({
     whatsApp: id,
