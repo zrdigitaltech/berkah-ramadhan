2f5cbb6cb2abc85b9fd5345a425fb26fdca97648
zikriramdani <zikriramdani@gmail.com>
2025-03-16 12:15:34 +0700

    Fixing Order

--- src/app/components/Navbar/index.js
+++ src/app/components/Navbar/index.js
@@ -7,7 +7,7 @@ import './cart.scss';
 import './navbar.scss';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getListKeranjangs, resetKeranjangInLocalStorage } from '@/app/redux/action/keranjangs/creator';
+import { getListKeranjangs } from '@/app/redux/action/keranjangs/creator';
 
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
@@ -30,7 +30,6 @@ const Index = () => {
 
   const fetchKeranjangs = async () => {
     setIsLoading(true);
-    resetKeranjangInLocalStorage();
     await dispatch(getListKeranjangs());
     setIsLoading(false);
   };
