d20a8fbe9600a2be204758d5005ebc91cdb12c69
zikriramdani <zikriramdani@gmail.com>
2025-03-16 13:15:34 +0700

    Fixing Order

--- src/app/beranda/index.js
+++ src/app/beranda/index.js
@@ -230,8 +230,8 @@ export default function Index() {
       {/* Kontent - Cemilan untukmu hari ini */}
 
       <section className="recipes recipes-today" ref={mainContent}>
+      <div style={{ height: '60px' }}></div>
         <div className="container mb-3">
-          {/* Tab Navigation */}
           <div className="align-items-center tabs-container">
             <h2 className="heading-2">Brand Pilihan UntukMu</h2>
 
